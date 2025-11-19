baecb0bbcd0f7bb72b12d5f55b65120e73ea8f35
Kelly Nkohla <kellynkohla0@gmail.com>
2025-11-19 02:32:38 +0200

    my portfolio

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="header-nav">
       <h1 className="logo">Kelly Thabo Nkohla</h1>
-      <ul className="nav-links">
-        <li><Link to="/" className="nav-link">Home</Link></li>
-        <li><Link to="/about" className="nav-link">About</Link></li>
-        <li><Link to="/education" className="nav-link">Education</Link></li>
-        <li><Link to="/experience" className="nav-link">Experience/Projects</Link></li>
-        <li><Link to="/technical" className="nav-link">Technical</Link></li>
+
+      {/* Desktop / hidden mobile menu */}
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <li><Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>About</Link></li>
+        <li><Link to="/education" className="nav-link" onClick={() => setMenuOpen(false)}>Education</Link></li>
+        <li><Link to="/experience" className="nav-link" onClick={() => setMenuOpen(false)}>Experience/Projects</Link></li>
+        <li><Link to="/technical" className="nav-link" onClick={() => setMenuOpen(false)}>Technical</Link></li>
       </ul>
+
+      {/* Hamburger menu icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 }
